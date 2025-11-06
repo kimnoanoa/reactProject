ce2bb67b88c150fd7fa160d302077d56170596f7
NOAH <noamom00@naver.com>
2025-11-06 15:53:21 +0900

    홈화면 추가

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import ProductDetail from "./pages/ProductDetail"; 
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} /> {/* 홈 */}
+        {/* 홈 */}
+        <Route path="/" element={<Home />} />
+        
+        {/* 상품 상세페이지 라우트 추가 */}
+        <Route path="/product/:id" element={<ProductDetail />} />
       </Routes>
     </BrowserRouter>
   );
